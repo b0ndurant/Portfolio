62df11dcd7fafc817101d95b00133958af8eb8b4
b0ndurant <martin.aurelien01@gmail.com>
2019-06-09 15:06:24 +0200

    fix activeClassName on navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,6 @@ import { NavHashLink as NavLink } from 'react-router-hash-link';
 
 class NavbarMenu extends Component {
     render() {
-
         return (
             <nav className="navbar navbar-expand-md navbar-dark fixed-top navbar-top">
                 <div className="mr-auto order-0">
@@ -21,16 +20,19 @@ class NavbarMenu extends Component {
                 <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
                     <ul className="navbar-nav ml-auto">
                         <li className="nav-item">
-                            <NavLink to="/#banner" isActive={()=> window.location.pathname + window.location.hash === "/#banner"} className="nav-link">Accueil</NavLink>
+                            <NavLink to="/#banner"     
+                            activeClassName="active"
+                            isActive={()=> window.location.hash === "#banner" || window.location.hash === ""}
+                            className="nav-link">Accueil</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to="/#Project" isActive={()=> window.location.pathname + window.location.hash === "/#Project"} className="nav-link" activeClassName="active" >Mes Projets</NavLink>
+                            <NavLink to="/#Project" isActive={()=> window.location.hash === "#Project"} className="nav-link" activeClassName="active" >Mes Projets</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to="/#toolsAndLlf" isActive={()=> window.location.pathname + window.location.hash === "/#toolsAndLlf"} className="nav-link" activeClassName="active">Langages & Outils</NavLink>
+                            <NavLink to="/#toolsAndLlf" isActive={()=> window.location.hash === "#toolsAndLlf"} className="nav-link" activeClassName="active">Langages & Outils</NavLink>
                         </li>
                         <li className="nav-item">
-                            <NavLink to="/#About" isActive={()=> window.location.pathname + window.location.hash === "/#About"} className="nav-link">A Propos</NavLink>
+                            <NavLink to="/#About" isActive={()=> window.location.hash === "#About"} className="nav-link">A Propos</NavLink>
                         </li>
                     </ul>
                 </div>
